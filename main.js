1017886c5889f27500e24d7a622acd0e9ce44b8c
Jordan Jueckstock <jjuecks@ncsu.edu>
2021-02-03 17:43:46 -0500

    First stab

--- main.js
+++ main.js
@@ -1,100 +1,89 @@
 'use strict';
 
 const am = require('am');
-const PublicSuffixList = require('publicsuffixlist');
 
 const { Session } = require('./lib/launch');
 const utils = require('./lib/utils');
 
 const CHROME_EXE = process.env.CHROME_EXE || '/usr/bin/google-chrome'
 const USE_XVFB = !!process.env.USE_XVFB
-const SPIDER_LINKS = +process.env.SPIDER_LINKS || 10
+const LOAD_COUNT = +process.env.SPIDER_LINKS || 10
 const NAV_TIMEOUT = 30.0 * 1000
+const LOITER_TIMEOUT = 30.0 * 1000
 const NAV_COMPLETE_EVENT = 'domcontentloaded'
 const MAX_CRAWL_TIME = 180.0 * 1000
+const COOLOFF_TIMEOUT = 3.0 * 1000
 
 
-const SamePublicSuffixPredicate = () => {
-    const psl = new PublicSuffixList();
-    psl.initializeSync();
+const doPerformanceCrawl = async (browser, targetUrl, visitCount, recordMetrics) => {
 
-    return (pageUrl, linkUrl) => {
-        return (linkUrl.protocol.startsWith('http')
-            && psl.domain(pageUrl.hostname) === psl.domain(linkUrl.hostname));
+    const allEventLogs = [];
+    let currentEventLog = [];
+    const rotateLog = () => {
+        allEventLogs.push(currentEventLog);
+        currentEventLog = [];
     };
-}
+    const log = (msg, ...args) => {
+        const ts = +new Date();
+        const deltaTs = currentEventLog.length > 0 ? (ts - currentEventLog[0].ts) : NaN;
 
-const LinkHarvester = (browser, linkPredicate) => {
-    return async () => {
-        const links = [];
-        for (const page of await browser.pages()) {
-            try {
-                const pageUrl = new URL(page.url());
-                for (const aTag of await page.$$('a[href]')) {
-                    const tagHref = await page.evaluate(a => a.href, aTag);
-                    try {
-                        const tagUrl = new URL(tagHref, pageUrl);
-                        if (linkPredicate(pageUrl, tagUrl)) {
-                            links.push({
-                                url: tagUrl.toString(),
-                                page: page,
-                            });
-                        }
-                    } catch (err) {
-                        console.error("link-harvesting href processing error:", err);
-                    }
-                }
-            } catch (err) {
-                console.error("link-harvesting page processing error:", err);
-            }
-        }
-        return links;
+        currentEventLog.push({
+            ts: ts,
+            deltaTs: deltaTs,
+            msg: msg,
+            args: args,
+        })
     }
+    const logWith = (msg, converter) => {
+        converter = converter || ((...args) => args.map(JSON.stringify));
+        return (...args) => {
+            log(msg, ...converter(...args));
+        }
+    };
+    const instrumentPage = async (page) => {
+        page.setDefaultTimeout(NAV_TIMEOUT);
+        page.on('domcontentloaded', logWith("domContentLoaded"));
+        page.on('load', logWith('load'));
+        page.on('pageerror', logWith('pageError', (error) => [error.message]));
+        page.on('framenavigated', logWith('frameNavigated', (frame) => [{name: frame.name(), url: frame.url()}]));
+        await page.setCacheEnabled(false);
+    };
+    
+    let page = await browser.newPage();
+    for (let visitIndex = 0; visitIndex < visitCount; ++visitIndex) {
+        log(`starting visit ${visitIndex}`);
+        await instrumentPage(page);
+
+        log(`navigating to ${targetUrl}`);
+        const loadEvent = page.waitForNavigation();
+        page.goto(targetUrl);
+
+        log(`waiting for load/timeout`);
+        await Promise.race([
+            loadEvent,
+            page.waitForTimeout(LOITER_TIMEOUT),
+        ])
+
+        log(`capturing page metrics post-load/loiter`, await page.metrics());
+
+        log(`cooling down/rotating logs/pages for next visit`);
+        const [ newPage, ..._ ] = await Promise.all([
+            browser.newPage(),
+            page.close(),
+            timeoutIn(COOLOFF_TIMEOUT),
+        ]);
+        page = newPage;
+        rotateLog();
+    }
+
+    await recordMetrics({
+        eventLogs: allEventLogs,
+    })
 };
-
-const doPathfinderCrawl = async (browser, seedUrl, spiderCount, recordNavUrl) => {
-    const harvestLinks = LinkHarvester(browser, SamePublicSuffixPredicate());
-    const page = await browser.newPage();
-
-    await page.goto(seedUrl, {
-        timeout: NAV_TIMEOUT,
-        waitUntil: NAV_COMPLETE_EVENT,
-    });
-
-    let lastUrl = page.url();
-    recordNavUrl(lastUrl);
-
-    const seenUrls = new Set();
-    seenUrls.add(seedUrl);
-    seenUrls.add(lastUrl);
-
-    const linkPool = [];
-    while (spiderCount > 0) {
-        const newLinks = await harvestLinks();
-	linkPool.push(...newLinks);
-        let navUrl = lastUrl, page;
-        while (seenUrls.has(navUrl)) {
-            if (linkPool.length === 0) {
-                throw Error("hey, we ran outta links...");
-            }
-            ({ url: navUrl, page } = utils.popRandomElement(linkPool));
-        }
-        await utils.closeOtherPages(browser, page);
-        await page.goto(navUrl, {
-            timeout: NAV_TIMEOUT,
-            waitUntil: NAV_COMPLETE_EVENT,
-            referer: lastUrl,
-        });
-        recordNavUrl(navUrl);
-        lastUrl = navUrl;
-        seenUrls.add(lastUrl);
-        --spiderCount;
-    }
-}
 
 const timeoutIn = (ms) => new Promise((resolve, _) => { setTimeout(resolve, ms) });
 
-am(async (seedUrl) => {
+am(async (targetUrl) => {
 
     const session = new Session();
     session.useBinary(CHROME_EXE).useTempProfile();
@@ -105,13 +94,19 @@ am(async (seedUrl) => {
     let exitStatus = 0;
     await session.run(async (browser) => {
         await Promise.race([
-            doPathfinderCrawl(browser, seedUrl, SPIDER_LINKS, (url) => {
-                console.log(url);
+            doPerformanceCrawl(browser, targetUrl, LOAD_COUNT, (metrics) => {
+                const { eventLogs } = metrics;
+                for (let i = 0; i < eventLogs.length; ++i) {
+                    const eventLog = eventLogs[i];
+                    for (const event of eventLog) {
+                        console.log(i, event.ts, event.deltaTs, event.msg, event.args);
+                    }
+                }
             }),
             timeoutIn(MAX_CRAWL_TIME),
         ])
     }).catch(err => {
-        console.error(`error browsing ${seedUrl}::`, err);
+        console.error(`error browsing ${targetUrl}::`, err);
         exitStatus = 1;
     });
 
